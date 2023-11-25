5e558d29e62ddfdd4bcf4b4b030207f645ba4561
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-11-25 16:25:21 +0000

    bookのedit,update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,8 +22,15 @@ class BooksController < ApplicationController
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
+   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.user)  
+    
+   end
   
    private
 
