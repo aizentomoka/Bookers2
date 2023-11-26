f6001090b8cb29571aaf6183f7ed549886cb401f
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-11-26 08:00:40 +0000

    フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,14 @@ class BooksController < ApplicationController
   end
   
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+      @book = Book.new(book_params)
+      @book.user_id = current_user.id
+      if @book.save
+        flash[:notice] = "You have created book successfully."
+        redirect_to book_path(@book.id)
+      else
+        render :new
+      end
   end
 
   def index
@@ -29,8 +32,12 @@ class BooksController < ApplicationController
   
    def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)  
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+        redirect_to book_path(@book)  
+    else 
+        render :edit
+    end
    end
   
    def destroy
