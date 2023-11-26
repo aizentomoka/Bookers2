78eddfee982d6d6adff7627405b0bbce586d8399
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-11-26 07:18:37 +0000

    ファイルの記述終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(@book.id)
   end
 
@@ -29,7 +30,7 @@ class BooksController < ApplicationController
    def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path(@book.user)  
+    redirect_to book_path(@book)  
    end
   
    def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
  
   def show
     @user = User.find(params[:id])
-    @books = @user.books    
+    @books = @user.books  
+   
   end
+  
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
     
   end
 
@@ -13,5 +15,22 @@ class UsersController < ApplicationController
     @users = User.all
     @user = current_user
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)  
+  end
+  
+  
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :introduction, :profile_image)
+    end
+      
+  
+  
   
 end
